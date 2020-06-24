0c45f495a252d48e0f817c60d58ae71eebdd91e6
Onur Osman Güler <guleronurosman@gmail.com>
2020-06-25 02:43:34 +0300

    Render a model

--- src/test/java/org/ogengine3d/MainGameLoop.java
+++ src/test/java/org/ogengine3d/MainGameLoop.java
@@ -1,16 +1,39 @@
 package org.ogengine3d;
 
 import org.ogengine3d.rengerEngine.DisplayManager;
+import org.ogengine3d.rengerEngine.Loader;
+import org.ogengine3d.rengerEngine.RawModel;
+import org.ogengine3d.rengerEngine.Renderer;
 
 public class MainGameLoop {
     public static void main(String[] args) {
         DisplayManager displayManager = new DisplayManager(1280, 700, "Our First Window");
         displayManager.createDisplay();
 
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        float[] vertices = { // Quad vertices
+                -0.5f, 0.5f, 0, // V0
+                -0.5f, -0.5f, 0, // V1
+                0.5f, -0.5f, 0, // V2
+                0.5f, 0.5f, 0 // V3
+        };
+
+        int[] indices = { // Quad indices
+                0, 1, 3, // Top left triangle (V0, V1, V3)
+                3, 1, 2 // Bottom right triangle (V3, V1, V2)
+        };
+
+        RawModel model = loader.loadToVAO(vertices, indices);
+
         while (!displayManager.isCloseRequested()) {
+            renderer.prepare();
+            renderer.render(model);
             displayManager.updateDisplay();
         }
 
+        loader.cleanUp();
         displayManager.closeDisplay();
     }
 }
