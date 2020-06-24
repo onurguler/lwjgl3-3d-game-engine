20536da09fde59be9d46541d0e77d2b7a1f76c01
Onur Osman Güler <guleronurosman@gmail.com>
2020-06-24 18:44:38 +0300

    Initial commit

---
+++ src/main/java/org/ogengine3d/App.java
@@ -0,0 +1,13 @@
+package org.ogengine3d;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
